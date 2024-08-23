8304f8193bded7b6106d6d8219b06ad123a277c7
Ibrahim9878 <talibliibrahim28@gmail.com>
2024-08-23 20:44:39 +0400

    Add project files.

---
+++ System Prog. Task 2 Copy File/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace System_Prog._Task_2_Copy_File
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ System Prog. Task 2 Copy File/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Win32;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace System_Prog._Task_2_Copy_File;
+
+
+public partial class MainWindow : Window, INotifyPropertyChanged
+{
+    private int value = 0;
+
+    public string FromText { get; set; }
+    public string ToText { get; set; }
+    public int Value { get => value; set { this.value = value; OnPropertyChanged(); } }
+    public int Maximum { get => maximum; set { maximum = value; OnPropertyChanged(); } }
+
+    public bool IsToButtonClicked { get; set; } = false;
+    public bool IsFromButtonClicked { get; set; } = false;
+    byte[]? buffer = null;
+    int NumBytesToRead = 0;
+    int NumByteRead = 0;
+    private int maximum = 0;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        Maximum = 1;
+        Value = 0;
+        DataContext = this;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+    private void FileClickButton(object sender, RoutedEventArgs e)
+    {
+        var btn = sender as Button;
+        OpenFileDialog fileDialog = new();
+        fileDialog.Filter = "Text files (*.txt)|*.txt";
+        fileDialog.ShowDialog();
+        if (btn == FromButton)
+        {
+            FromBox.Text = fileDialog.FileName;
+            FromText = fileDialog.FileName;
+        }
+        else
+        {
+            IsToButtonClicked = true;
+            ToBox.Text = fileDialog.FileName;
+            ToText = fileDialog.FileName;
+        }
+
+    }
+
+    private void CopyClickButton(object sender, RoutedEventArgs e)
+    {
+
+        if (!IsToButtonClicked)
+        {
+            if (!File.Exists(ToBox.Text))
+            {
+                using (File.Create(ToBox.Text)) { };
+            }
+            ToText = ToBox.Text;
+        }
+        if (!IsFromButtonClicked)
+        {
+            FromText = FromBox.Text;
+        }
+        int n = 0;
+        using (FileStream fs = new FileStream(FromText, FileMode.Open, FileAccess.Read))
+        {
+            buffer = new byte[fs.Length];
+            NumBytesToRead = Convert.ToInt32(fs.Length);
+            NumByteRead = 0;
+
+            while (NumBytesToRead > 0)
+            {
+                n = fs.Read(buffer, NumByteRead, NumBytesToRead);
+                NumBytesToRead -= n;
+                NumByteRead += n;
+            }
+        }
+        Maximum = n;
+        new Thread(() =>
+        {
+            using (FileStream fs = new FileStream(ToText, FileMode.Create, FileAccess.Write))
+            {
+                int count = 0;
+
+                while (NumByteRead > 0)
+                {
+                    fs.Write(buffer, count, 1);
+                    count++;
+                    NumByteRead--;
+                    Value += 1;
+                    fs.Flush();
+                    Thread.Sleep(50);
+                }
+
+            }
+        }).Start();
+
+    }
+}
